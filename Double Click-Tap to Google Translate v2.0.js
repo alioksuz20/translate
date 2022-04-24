cc891646149f4deb021569b0ea46d9276a026a71
alioksuz20 <98310928+alioksuz20@users.noreply.github.com>
2022-04-24 08:52:06 +0300

    Update Double Click-Tap to Google Translate v2.0.js

--- Double Click-Tap to Google Translate v2.0.js
+++ Double Click-Tap to Google Translate v2.0.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Double Click/Tap to Google Translate v2
 // @namespace    http://tampermonkey.net/
-// @version      2.06
+// @version      2.07
 // @description  try to take over the world!
 // @author       You
 
